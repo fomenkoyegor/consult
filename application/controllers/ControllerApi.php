2cb641fee5786dd1f37de9a701c6a27b20648e92
fomenkoyegor <fomenkoyegor@gmail.com>
2018-06-29 03:30:50 +0300

    api/openConsult

--- application/controllers/ControllerApi.php
+++ application/controllers/ControllerApi.php
@@ -14,31 +14,44 @@ class ControllerApi extends Controller
     }
 
 
+    /**
+     * @throws Exception
+     */
     public function action_openConsult()
     {
         $res = ['status' => 'ok'];
 
         try {
 
-            $db = ModuleDatabaseConnection::instance();
-            $auth = ModuleTokenAuth::instance();
-            $token = $_POST['token'];
-            $time = $_POST['time'];
-            $user_id = $auth->getUser($token)['id'];
 
-            if (empty($token) || $auth->isAuth($token))
+            if (empty($_POST['token']) || ModuleTokenAuth::instance()->isAuth($_POST['token'])) {
+
+                ModuleDatabaseConnection::instance()->consults->insert([
+                    'starttime' => $_POST['time'],
+                    'user_id' => ModuleTokenAuth::instance()->getUser($_POST['token'])['id']
+                ]);
+            } else {
                 throw new Exception('auth fail');
-            if (empty($time))
+            }
+
+
+            if (empty($_POST['time']))
                 throw new Exception('not all params');
 
-            $db->consults->insert([
-                'starttime'=>$time,
-                'user_id'=>$user_id
-            ]);
 
         } catch (Exception $e) {
-            $res['msg']=$e->getMessage();
+            $res['msg'] = $e->getMessage();
         }
+
+        $user_id = ModuleTokenAuth::instance()->getUser($_POST['token'])['id'];
+        $consults = ModuleDatabaseConnection::instance()->consults->all();
+        $res['user_id'] = $user_id;
+        $res['time'] = $_POST['time'];
+        $res['consults'] = $consults;
+//        ModuleDatabaseConnection::instance()->consults->insert([
+//            "starttime"=>$_POST["time"],
+//            "user_id"=>$user_id
+//        ]);
 
         $this->response(json_encode($res));
     }
