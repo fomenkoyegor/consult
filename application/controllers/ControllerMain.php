6376ab33b411757348612e0099d43be1381a92d3
fomenkoyegor <fomenkoyegor@gmail.com>
2018-07-01 17:52:55 +0300

    echo

--- application/controllers/ControllerMain.php
+++ application/controllers/ControllerMain.php
@@ -18,9 +18,10 @@ class ControllerMain extends Controller
     public function action_index()
     {
 
-        $view = new View("main");
-        $view->useTemplate();
-        $this->response($view);
+//        $view = new View("main");
+////        $view->useTemplate();
+////        $this->response($view);
+        echo "re";
 
     }
 
