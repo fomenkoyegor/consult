6a1f865755dc75d6b6b7b34c283d756ea16d897b
fomenkoyegor <fomenkoyegor@gmail.com>
2018-07-02 01:16:57 +0300

    echo

--- application/config/database.php
+++ application/config/database.php
@@ -1,8 +1,8 @@
 <?php
 return [
-  "login"=>"root",
-  "pass"=>"",
-  "dbname"=>"arturapi",
+  "login"=>"fcii2x0y58slvant",
+  "pass"=>"jescr2kcmpye33pb",
+  "dbname"=>"bw4wt91t0pisgmkc",
   "charset"=>"utf8",
-  "host"=>"127.0.0.1"
+  "host"=>"zwgaqwfn759tj79r.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"
 ];
